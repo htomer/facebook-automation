a466b5f885f8cd603c161112bb7fe2e31c1ce4fc
Tomer Hanochi <64750051+htomer@users.noreply.github.com>
2023-10-15 01:56:49 +0300

    Config improvements

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from facebook_automation.facebook import Credentials, Facebook
 import yaml
 
 
-class Config(yaml.YAMLObject):
-    yaml_tag = u'!Config'
+class Config():
 
-    def __init__(self, keyword: str, count: str, email: str, password: str):
-        self.email = email
-        self.password = password
+    def __init__(self, keyword: str, count: str, credentials):
         self.keyword = keyword
         self.count = count
+        self.credentials = Credentials(**credentials)
 
     def __repr__(self) -> str:
-        return "%s(keyword=%r, count=%r)" % (self.__class__.__name__, self.keyword, self.count)
+        return "%s(keyword=%r, count=%r, %r)" % (self.__class__.__name__, self.keyword, self.count, self.credentials)
 
 
 def save_group_names_to_file(group_names: list, file_name: str = "group_names.txt"):
@@ -23,15 +21,16 @@ def save_group_names_to_file(group_names: list, file_name: str = "group_names.tx
 
 
 def main():
-    # Load options file.
-    config = yaml.load(open("config.yml"), Loader=yaml.Loader)
-    print(config)
 
-    # Load facebook credentials from config.
-    creds = Credentials(config.email, config.password)
+    # Load options file.
+    with open('config.yml', 'r') as yaml_file:
+        config_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+    # Load options into config intance.
+    config = Config(**config_data)
 
     # Initialize the Facebook class
-    with Facebook(creds) as facebook:
+    with Facebook(config.credentials) as facebook:
         # Login to Facebook.
         facebook.login()
 
